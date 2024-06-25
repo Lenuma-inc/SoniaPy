378015ca5d386693e5dc77346741b77540156825
Boris Yumankulov <boria138@altlinux.org>
2024-06-25 15:44:09 +0500

    add more jokes

--- voice.py
+++ voice.py
@@ -1,31 +1,29 @@
-import torch
-import sounddevice as sd
-import time
 import os
+import torch
+import time
+import sounddevice as sd
 
-language = 'ru'
-model_id = 'v3_1_ru'
 device = torch.device('cpu')
-local_file = 'model.pt'
+torch.set_num_threads(4)
+local_file = "model.pt"
 
 if not os.path.isfile(local_file):
-    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/v3_1_ru.pt',
-                                   local_file)  
+    torch.hub.download_url_to_file("https://models.silero.ai/models/tts/ru/v4_ru.pt",
+                                   local_file)
 
-model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
-model.to(device)  # gpu or cpu
+model = torch.package.PackageImporter(
+    local_file).load_pickle("tts_models", "model")
+model.to(device)
+
 sample_rate = 48000
 speaker = 'baya'
-put_accent = True
-put_yo = True
 
-def bot_speak(what):
-	audio = model.apply_tts(text=what,
-                        speaker=speaker,
-                        sample_rate=sample_rate,
-                        put_accent=put_accent,
-                        put_yo=put_yo)
 
-	sd.play(audio, sample_rate * 1.05)
-	time.sleep((len(audio) / sample_rate) + 0.5)
-	sd.stop()
+def bot_speak(text):
+    audio = model.apply_tts(text=text,
+                            speaker=speaker,
+                            sample_rate=sample_rate)
+
+    sd.play(audio, sample_rate * 1.05)
+    time.sleep((len(audio) / sample_rate) + 0.5)
+    sd.stop()
